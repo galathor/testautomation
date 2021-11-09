85f264f1f9b35944c6edb868ff8319a1b6b96227
z004e0fx <z004e0fx@EVT03080NB.ad001.siemens.net>
2021-11-09 14:25:38 +0300

    calisir durum

--- src/test/java/selenium/PetClinicSeleniumTest.java
+++ src/test/java/selenium/PetClinicSeleniumTest.java
@@ -1,27 +1,22 @@
 package selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class PetClinicSeleniumTest {
 
-public class PetClinicTestRunner {
-	
 	WebDriver driver;
 	WebElement element;
-	
+
 	@Test
 	public void startUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8088/");
 	}
-	
-	
-	
 
 }
