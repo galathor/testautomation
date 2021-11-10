d68ce774d0884b8d76a8601d9ec4f0551113683f
z004e0fx <z004e0fx@EVT03080NB.ad001.siemens.net>
2021-11-10 11:28:10 +0300

    cucumber update

---
+++ src/test/java/cucumber/StepDefinition.java
@@ -0,0 +1,42 @@
+package cucumber;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StepDefinition {
+	
+	WebDriver driver;
+	WebElement searchbox;
+	
+	@Given("launch the browser")
+	public void launch_the_browser() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@When("Hit Petclinic on the browser")
+	public void hit_petclinic_on_the_browser() {
+		driver.get("http://localhost:8088/");
+	}
+	@Then("Enter {string} in the search box")
+	public void enter_in_the_search_box(String string) {
+		driver.findElement(By.cssSelector("a[href=\"/owners/find\"]")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("a[href=\"/owners/find\"]")).getText(),"FIND OWNERS" );
+		driver.findElement(By.id("lastName")).sendKeys(string);
+	}
+	@Then("Search it")
+	public void search_it() {
+		driver.findElement(By.cssSelector("input[id=\"lastName\"]")).sendKeys(Keys.ENTER);
+	}
+	
+
+}
---
+++ src/test/resources/features/petclinic/petclinic.feature
@@ -0,0 +1,9 @@
+Feature: PetClinic Operations
+
+	Background: To launch the browser
+			Given launch the browser
+		
+	Scenario: Search for Karaca in Petclinic
+		When Hit Petclinic on the browser
+		Then Enter "Karaca" in the search box
+		Then Search it 
